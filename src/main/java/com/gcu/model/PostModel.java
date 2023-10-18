d14b15e4197b1666f71f6cadfe468e02326d9931
Alejandro Barranco-Leyte <alejandrobarranco01@gmail.com>
2023-10-18 15:42:37 -0700

    Update PostModel.java

--- src/main/java/com/gcu/model/PostModel.java
+++ src/main/java/com/gcu/model/PostModel.java
@@ -1,14 +1,41 @@
 package com.gcu.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Entity
 public class PostModel {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String author;
-    private String text;
-    private int postNumber;
+	private String author;
+	private String text;
 
-    // Constructors, getters, and setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
 }
