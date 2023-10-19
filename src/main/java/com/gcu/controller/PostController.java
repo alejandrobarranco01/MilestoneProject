73c3d0b2a081ec5ca0432675506a4af7e6376ea7
Alejandro Barranco-Leyte <alejandrobarranco01@gmail.com>
2023-10-18 17:01:37 -0700

    Updated logged in page, created postcontroller and post model but no implementations yet

---
+++ src/main/java/com/gcu/controller/PostController.java
@@ -0,0 +1,16 @@
+package com.gcu.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.gcu.model.PostModel;
+
+@Controller
+@RequestMapping("/create")
+public class PostController {
+	
+}
--- src/main/java/com/gcu/model/PostModel.java
+++ src/main/java/com/gcu/model/PostModel.java
@@ -7,27 +7,15 @@ import javax.persistence.Id;
 
 @Entity
 public class PostModel {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-
-	private String author;
+	private String title;
 	private String text;
-
-	public Long getId() {
-		return id;
+	
+	public String getTitle() {
+		return title;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
 	public String getText() {
