25cdd35e0d6ad8fbc5099dfd4d8d6706143171cd
Alejandro Barranco-Leyte <alejandrobarranco01@gmail.com>
2023-10-22 14:50:14 -0700

    Implemented a Security Service on the Login Controller

---
+++ src/main/java/com/gcu/business/SecurityBusinessService.java
@@ -0,0 +1,12 @@
+package com.gcu.business;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityBusinessService {
+	public boolean authenticate(String username, String password) {
+		// Incorporate business logic to verify information with database
+		
+		return true;
+	}
+}
