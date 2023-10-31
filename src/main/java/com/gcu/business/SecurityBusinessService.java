f7f4c47b25ef1d0c4ed05d62fe1e23412bfef8b5
Alejandro Barranco-Leyte <alejandrobarranco01@gmail.com>
2023-10-30 17:40:04 -0700

    Added comments

--- src/main/java/com/gcu/business/SecurityBusinessService.java
+++ src/main/java/com/gcu/business/SecurityBusinessService.java
@@ -7,20 +7,36 @@ import com.gcu.data.UsersDataService;
 import com.gcu.data.entity.UserEntity;
 
 /**
- * This class should authenticate and create accounts
+ * Service class handling user authentication and account creation operations.
  */
 @Service
-public class SecurityBusinessService implements SecurityBusinessServiceInterface{
+public class SecurityBusinessService implements SecurityBusinessServiceInterface {
 	@Autowired
 	UsersDataService service;
-	
+
+	/**
+	 * Authenticates the user based on the provided email and password.
+	 * 
+	 * @param email    The email address of the user.
+	 * @param password The password associated with the user's account.
+	 * @return True if the provided email and password match a user's record,
+	 *         indicating successful authentication; false otherwise.
+	 */
 	public boolean authenticate(String email, String password) {
 		return service.verifyLogin(email, password);
 	}
 
+	/**
+	 * Creates a new user account with the provided email, username, and password.
+	 * 
+	 * @param email    The email address of the user.
+	 * @param username The desired user name for the user.
+	 * @param password The password for the user's account.
+	 * @return True if the account was created successfully, false if a user with
+	 *         the same user name or email already exists.
+	 */
 	public boolean createAccount(String email, String username, String password) {
-        // You can validate the inputs here if needed
-        UserEntity user = new UserEntity(username, email, password);
-        return service.createAccount(user);
-    }
+		UserEntity user = new UserEntity(username, email, password);
+		return service.createAccount(user);
+	}
 }
--- src/main/java/com/gcu/data/repository/UsersRepository.java
+++ src/main/java/com/gcu/data/repository/UsersRepository.java
@@ -1,15 +1,24 @@
 package com.gcu.data.repository;
 
-import java.util.List;
-
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.gcu.data.entity.UserEntity;
-
+/**
+ * This interface represents a repository for managing user data.
+ * It provides methods for verifying user login credentials.
+ */
 @Repository
-public interface UsersRepository extends CrudRepository<UserEntity, Long>{
+public interface UsersRepository extends CrudRepository<UserEntity, Long> {
+	 /**
+     * Verifies user login credentials based on the provided email and password.
+     * 
+     * @param email The email address of the user.
+     * @param password The password associated with the user's account.
+     * @return An integer representing the count of matching user records found in the database.
+     *         If a matching user is found, the count will be greater than 0; otherwise, it will be 0.
+     */
 	@Query("SELECT COUNT(*) FROM USERS WHERE EMAIL = :email AND PASSWORD = :password")
     public int verifyLogin(String email, String password);
 	
